14170d87917c26ca74770be800822d86a516cb33
Varga Marci <martonv92@gmail.com>
2017-11-26 15:44:55 +0100

    Had no idea how to do this. Bálint helped.

--- bowling.py
+++ bowling.py
@@ -1,46 +1,42 @@
+def get_value(char):
+    try:
+        return int(char)
+    except ValueError:
+        if char.upper() == 'X' or char == '/':
+            return 10
+        elif char == '-':
+            return 0
+        raise ValueError()
+
+
 def score(game):
-    result = 0
-    frame = 1
+    result, frame = 0, 1
+    frame_limit = 10
     in_first_half = True
+
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
-        else:
+        try:
+            is_spare = (game[i + 1] == "/")
+        except IndexError:
+            is_spare = False
+
+        if not is_spare:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i + 1])
-                if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
-                else:
-                    result += get_value(game[i + 2])
-        last = get_value(game[i])
+
+            if game[i].upper() == "X":
+                result += get_value(game[i + 1]) + get_value(game[i + 2])
+                in_first_half = False
+
+            if game[i] == "/":
+                in_first_half = False
+        else:
+            result += get_value(game[i + 2])
+
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
-        else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
-            frame += 1
+        in_first_half = not in_first_half
+
+        if frame > frame_limit:
+            break
+
     return result
-
-
-def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
-    else:
-        raise ValueError()
