cb2033e320973504b2b6028d8ad33a94818cb9d9
Varga Marci <martonv92@gmail.com>
2017-11-26 15:53:47 +0100

    added comments

--- bowling.py
+++ bowling.py
@@ -1,11 +1,15 @@
+# the function that is used in another function should be defined first.
 def get_value(char):
+    # if char can be converted to int, then it's a number.
     try:
         return int(char)
     except ValueError:
+        # both "/" and "X" mean 10
         if char.upper() == 'X' or char == '/':
             return 10
         elif char == '-':
             return 0
+        # if none of the above can run, ValueError is raised
         raise ValueError()
 
 
@@ -15,6 +19,7 @@ def score(game):
     in_first_half = True
 
     for i in range(len(game)):
+        # is_spare helps us stay in the index range
         try:
             is_spare = (game[i + 1] == "/")
         except IndexError:
@@ -23,6 +28,7 @@ def score(game):
         if not is_spare:
             result += get_value(game[i])
 
+            # the .upper method enables us to evaluate both "x" and "X"
             if game[i].upper() == "X":
                 result += get_value(game[i + 1]) + get_value(game[i + 2])
                 in_first_half = False
@@ -36,6 +42,7 @@ def score(game):
             frame += 1
         in_first_half = not in_first_half
 
+        # this is where the game ends.
         if frame > frame_limit:
             break
 
